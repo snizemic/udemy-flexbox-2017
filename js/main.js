25d2451995b7bbbe76535cdb71238e0fd8f043d7
Michal Snizek <snizemic@gmail.com>
2017-05-29 23:19:15 +0200

    pokusy regex

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ var askMom = function () {
 };
 
 askMom();
+
+let foo = {
+  1: 'Steinberg',
+  2: 'Ableton',
+  3: 'M-Audio'
+}
+
+console.log(Object.keys(foo));
+
+let src = 'http://dealer.disk.cz/prodejci?field_vyrobci_prodejce_tid_entityreference_filter=12&_ga=2.26721917.1534467111.1496004265-740443567.1458997491';
+
+let regex = /filter=(.*)&/;
+console.log(src.match(regex));
+
+
+let num = 1;
+
+for (let key in foo) {
+  if (key === num.toString()) {
+    console.log(foo[key]);
+  }
+}
